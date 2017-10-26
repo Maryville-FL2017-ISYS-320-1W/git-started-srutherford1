a8d536d67c70914aa16a1670910a7094c7b2bc69
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-10-25 22:42:41 -0400

    Update AboutYou.java

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -1,13 +1,13 @@
-
 public class AboutYou {
 
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("Samantha Rutherford");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("It is a class needed for my major. lol.");
 	}
 
 }
+
